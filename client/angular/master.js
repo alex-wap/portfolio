f7c3820b28e0405d36113c9f060e0ad6e0145d78
Alex W <wapniarski@gmail.com>
2016-11-29 17:58:14 -0800

    updated skills

--- client/angular/master.js
+++ client/angular/master.js
@@ -30,13 +30,12 @@ app.config(function ($routeProvider) {
 app.factory('MainFactory', function() {
     var factory = {};
 
-    factory.languages = ["JavaScript","Python","Swift"];
-    factory.frameworks = ["Node.js","AngularJS","Express.js","Flask","Socket.io","Scrum"]; // ,"React.js"
-    factory.databases = ["MySQL", "MongoDB", "SQL", "NoSQL"];  
-    factory.tools = ["jQuery","Git", "AWS EC2","AJAX","Unix/Linux","Xcode","Nginx"];
-    factory.design = ["HTML5","CSS3","Bootstrap","Materialize","Photoshop","Illustrator"];
-    factory.misc = ["PTC Creo","AutoCAD","SolidWorks","SketchUp","3D Studio Max","Microsoft Office Suite","Ping Pong"]; 
-
+    factory.languages = ["JavaScript","Python","Swift", "HTML5","CSS3"];
+    factory.frontend = ["AngularJS","jQuery","AJAX","Bootstrap","Materialize"]; // ,"React.js"
+    factory.backend = ["Node.js","Flask","Express.js","Socket.IO","AWS EC2","Nginx"];
+    factory.database = ["MySQL", "MongoDB", "SQL", "NoSQL"];
+    factory.software = ["Git","Xcode","Unix","Linux","Photoshop","Illustrator"];
+    factory.misc = ["Scrum","AutoCAD","PTC Creo","SolidWorks","3D Studio Max","Microsoft Office Suite","Ping Pong"]; 
     // factory.index = function(callback) {
     //     $http.get('/scores').then(function(response) {
     //         callback(response.data);
@@ -63,10 +62,10 @@ app.controller('MainController', function($scope, MainFactory) {
     // $scope.scores = [{name:'Alex',speed:100,time:10},{name:'Elliot',speed:10,time:20},{name:'Phil',speed:600,time:5}]
 
     $scope.languages = MainFactory.languages;
-    $scope.frameworks = MainFactory.frameworks;
-    $scope.databases = MainFactory.databases;
-    $scope.tools = MainFactory.tools;
-    $scope.design = MainFactory.design;
+    $scope.frontend = MainFactory.frontend;
+    $scope.backend = MainFactory.backend;
+    $scope.database = MainFactory.database;
+    $scope.software = MainFactory.software;
     $scope.misc = MainFactory.misc;
 
 })
