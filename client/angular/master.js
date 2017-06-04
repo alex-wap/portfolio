d1bf103799d0cf00e8a2d8edd9c1ab86a502224e
Alex W <wapniarski@gmail.com>
2017-06-04 00:29:34 -0700

    updated text and skills

--- client/angular/master.js
+++ client/angular/master.js
@@ -30,11 +30,11 @@ app.config(function ($routeProvider) {
 app.factory('MainFactory', function() {
     var factory = {};
 
-    factory.languages = ["JavaScript","Python","Swift", "HTML5","CSS3"]; // ,"JSX"
+    factory.languages = ["C#","JavaScript","Python","Swift","HTML5","CSS3"]; // ,"JSX"
     factory.frontend = ["AngularJS","jQuery","AJAX","Bootstrap","Materialize"]; // ,"React.js","Webpack"
-    factory.backend = ["Node.js","Django","Flask","Express.js","Socket.IO","AWS EC2","Nginx","RESTful"];
+    factory.backend = [".NET","Node.js","Django","Flask","Express.js","Socket.IO","AWS EC2","Nginx","RESTful"];
     factory.database = ["MySQL", "MongoDB", "SQL", "NoSQL", "Mongoose", "MAMP"];
-    factory.software = ["Git","Xcode","Unix","Linux","Photoshop","Illustrator"];
+    factory.software = ["Git","Visual Studio","Xcode","Unix","Linux","Windows","Photoshop","Illustrator"];
     factory.misc = ["Scrum","AutoCAD","PTC Creo","SolidWorks","3D Studio Max","Ping Pong"]; 
 
     return factory;
